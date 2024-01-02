ff65f40b185d52d4318931a270719fc2b2815ac2
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2024-01-02 19:46:35 +0800

    Create title.js

---
+++ title.js
@@ -0,0 +1,24 @@
+/*
+
+PROVIDED SPOOKHOST (https://spookhost.xyz/)
+AND SPOOKY SERVICES (https://spookysrv.com/)
+BY SPOOKY KIPPER (https://spooky.hk/)
+DO NOT REMOVE CREDITS
+
+THIS CODE IS OPEN SOURCED AT 
+https://github.com/SpookyServices/TitledVP/
+*/
+document.querySelector("#lnkHeaderHome").setAttribute('href', `?`);
+if (document.getElementById('pageHeading')) {
+    if ((typeof panel_name != "undefined") && (typeof panel_name == "string")) {
+        document.title = document.getElementById('pageHeading').innerHTML + " - " + panel_name;
+    } else {
+        document.title = document.getElementById('pageHeading').innerHTML + " - Control Panel";
+    }
+} else {
+    if ((typeof panel_name != "undefined") && (typeof panel_name == "string")) {
+        document.title = panel_name;
+    } else {
+        document.title = "Control Panel";
+    }
+}
